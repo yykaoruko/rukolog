212af93ee47cf2a353072c4bbbc599375811c15b
yamamoto kaoruko <kaoruko@diffeasy.com>
2019-09-07 15:56:44 +0900

    feat: setup Jest #1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,10 @@ const config: Configuration = {
   mode: 'universal',
   srcDir: 'app/',
   buildModules: ['@nuxt/typescript-build'],
+  typescript: {
+    typeCheck: true,
+    ignoreNotFoundWarnings: true,
+  },
 };
 
 export default config;
