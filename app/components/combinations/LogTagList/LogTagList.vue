15d1964c569e0214bbba38e8eddf873c663dd8c0
yamamoto kaoruko <kaoruko@diffeasy.com>
2019-09-08 16:18:31 +0900

    feat: add LogTagList component

---
+++ app/components/combinations/LogTagList/LogTagList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="log-tag-list">
+    <template v-for="(item, index) in tags">
+      <li :key="index" class="log-tag-list__item">
+        <app-label :label="item" />
+      </li>
+    </template>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import AppLabel from '@/components/basics/AppLabel/AppLabel.vue';
+export default Vue.extend({
+  name: 'LogTagList',
+  components: {
+    AppLabel,
+  },
+  props: {
+    tags: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.log-tag-list {
+  color: inherit;
+}
+</style>
