81de78818ec16b4a409a57c4fc2d08150f82087f
yamamoto kaoruko <kaoruko@diffeasy.com>
2019-09-08 16:08:51 +0900

    feat: add LogDescription component

---
+++ app/components/basics/LogDescription/LogDescription.vue
@@ -0,0 +1,24 @@
+<template>
+  <p class="log-description">
+    {{ text }}
+  </p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'LogDescription',
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.log-description {
+  color: inherit;
+}
+</style>
