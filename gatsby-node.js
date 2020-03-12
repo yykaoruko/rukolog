44ec5d095be1d2a412be878cf27af5eff7ed853e
Kaoruko <yykaoruko@gmail.com>
2020-03-12 09:02:52 +0900

    refactor: 記事が0件のときページをgenerateしない

--- gatsby-node.js
+++ gatsby-node.js
@@ -39,49 +39,51 @@ exports.createPages = async ({ graphql, actions }) => {
 
   const postsEdges = markdownQueryResult.data.allMarkdownRemark.edges
 
-  // generate post page
-  postsEdges.forEach((edge, index) => {
-    createPage({
-      path: `/posts/${edge.node.frontmatter.slug}`,
-      component: postPage,
-      context: {
-        id: edge.node.id,
-      },
+  if (postsEdges.length > 0) {
+    // generate post page
+    postsEdges.forEach((edge, index) => {
+      createPage({
+        path: `/posts/${edge.node.frontmatter.slug}`,
+        component: postPage,
+        context: {
+          id: edge.node.id,
+        },
+      })
     })
-  })
 
-  // generate posts page to paginate
-  const postsPerPage = 12
-  const numPages = Math.ceil(postsEdges.length / postsPerPage)
-  Array.from({ length: numPages }).forEach((_, i) => {
-    createPage({
-      path: i === 0 ? `/` : `/${i + 1}`,
-      component: path.resolve("./src/templates/postsPage/index.js"),
-      context: {
-        limit: postsPerPage,
-        skip: i * postsPerPage,
-        numPages,
-        currentPage: i + 1,
-      },
+    // generate posts page to paginate
+    const postsPerPage = 12
+    const numPages = Math.ceil(postsEdges.length / postsPerPage)
+    Array.from({ length: numPages }).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/` : `/${i + 1}`,
+        component: path.resolve("./src/templates/postsPage/index.js"),
+        context: {
+          limit: postsPerPage,
+          skip: i * postsPerPage,
+          numPages,
+          currentPage: i + 1,
+        },
+      })
     })
-  })
 
-  // generate tag page
-  const allTags = postsEdges
-    .map(edge => edge.node.frontmatter.tags)
-    .reduce((a, c) => {
-      return a.concat(c)
-    }, [])
-  tags = Array.from(new Set(allTags))
-  tags.forEach(tag => {
-    createPage({
-      path: `/tags/${tag}/`,
-      component: tagPage,
-      context: {
-        tag: tag,
-      },
+    // generate tag page
+    const allTags = postsEdges
+      .map(edge => edge.node.frontmatter.tags)
+      .reduce((a, c) => {
+        return a.concat(c)
+      }, [])
+    tags = Array.from(new Set(allTags))
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${tag}/`,
+        component: tagPage,
+        context: {
+          tag: tag,
+        },
+      })
     })
-  })
+  }
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
