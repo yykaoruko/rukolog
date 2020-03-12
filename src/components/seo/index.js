22ff48f09f89649f937262a8a1a3a686b89da6b3
Kaoruko <yykaoruko@gmail.com>
2020-03-12 11:04:31 +0900

    refactor: OGP設定

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import ogpImage from "../../images/heart.png"
+import ogpImage from "../../images/OGP.png"
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
